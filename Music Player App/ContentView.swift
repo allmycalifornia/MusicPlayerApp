3da2af869acb8102c4d09197c58c0e55eb0565f8
Boris Kravchenko <allmycalifornia@gmail.com>
2024-12-23 15:20:53 +0300

    Initial Commit

---
+++ Music Player App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Music Player App
+//
+//  Created by Борис Кравченко on 23.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Music Player App/Music_Player_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_Player_AppApp.swift
+//  Music Player App
+//
+//  Created by Борис Кравченко on 23.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Music_Player_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
