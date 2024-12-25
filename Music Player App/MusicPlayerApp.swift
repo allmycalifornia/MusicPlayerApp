a9050cc48c20ae2f8cc7c4f26a492c96d139e73c
Boris Kravchenko <allmycalifornia@gmail.com>
2024-12-25 18:16:42 +0300

    added audio playback background mode support

--- Music Player App/MusicPlayerApp.swift
+++ Music Player App/MusicPlayerApp.swift
@@ -5,13 +5,39 @@
 //  Created by Борис Кравченко on 23.12.2024.
 //
 
+//import SwiftUI
+//
+//@main
+//struct MusicPlayerApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            PlayerView()
+//        }
+//    }
+//}
+
 import SwiftUI
+import AVFoundation
 
 @main
 struct MusicPlayerApp: App {
+    init() {
+        configureAudioSession()
+    }
+    
     var body: some Scene {
         WindowGroup {
             PlayerView()
+        }
+    }
+    
+    private func configureAudioSession() {
+        do {
+            let audioSession = AVAudioSession.sharedInstance()
+            try audioSession.setCategory(.playback, mode: .default, options: [])
+            try audioSession.setActive(true)
+        } catch {
+            print("Failed to set up audio session: \(error)")
         }
     }
 }
